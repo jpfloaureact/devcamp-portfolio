573aa4bfcdd62d14c4b4345d71d10ff61c1e1439
JP Florez <jpflorez@MacBook-Pro-2.local>
2018-07-24 14:11:54 -0400

    destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,10 +21,10 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_items = Portfolio.find(params[:id])
-  end
+end
 
-  def update
-    @portfolio_items = Portfolio.find(params[:id])
+def update
+  @portfolio_items = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'record was successfully updated.' }
@@ -32,13 +32,23 @@ class PortfoliosController < ApplicationController
       else
         format.html { render :edit }
         format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
-      end
     end
   end
+end
 
 def show
   @portfolio_items = Portfolio.find(params[:id])
 end
 
+def destroy
+  #perform look-up
+  @portfolio_items = Portfolio.find(params[:id])
+  #destroy record
+  @portfolio_items.destroy
+  #redirect
+  respond_to do |format|
+    format.html { redirect_to portfolios_url, notice: 'portfolio was successfully destroyed.' }
+  end
+end
 
 end
